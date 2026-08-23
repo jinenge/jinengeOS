c13af6ffe16a7d1fc462320983d660bda814f38f
进恩哥资源仓库 <119514614+jinenge@users.noreply.github.com>
2026-08-23 15:02:14 +0800

    Add files via upload

--- Update.js
+++ Update.js
@@ -1,7 +1,7 @@
 //设置当前版本数据
-var versionName = "2.1";
-var versionCode = 260413;
-var buildDate = "2026年04月13日";
+var versionName = "2.2";
+var versionCode = 260603;
+var buildDate = "2026年06月03日";
 //函数：获取最新版本数据
 function checkUpdate() {
     var getUpdateData = new XMLHttpRequest();
